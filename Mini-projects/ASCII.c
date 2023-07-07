326bc0e39f56259a1f6d1990cff48474c6f167cd
I-am-Programming-the-World <zaniarkarimi@chmail.ir>
2023-07-07 12:59:39 +0330

    Added the source code for 'ASCII code generator (sequential)'.

---
+++ Mini-projects/ASCII.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main( void )
+{
+	int i;
+	for( i = 33; i < 256; ++i ) {
+		printf("%d = %c\n", i, i );
+	}//end for
+}//end main
+/*This program was developed by ©I-am-Programming-the-World*/
