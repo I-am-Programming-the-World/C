5a0de488e9ddc3413e6f9e25751fec54303223c0
Zaniar Karimi <105171640+I-am-Programming-the-World@users.noreply.github.com>
2023-12-05 23:39:40 +0330

    Very basic ASCII values generator.

---
+++ Mini-projects/print out all ASCII values.c
@@ -0,0 +1,10 @@
+//This prints out all ASCII values:
+#include<stdio.h>
+int main( void )
+{
+    int i;
+    for( i = 0; i < 256; ++i ) {
+        printf("%d %c\t",i,i);
+    }
+}
+/*This program was developed by ©I-am-Programming-the-World*/
